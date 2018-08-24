ccd229d2686ed61b0fdf821da75f8aa97afe3600
Chace Hartwell <chace.l.hartwell@icloud.com>
2018-08-24 13:20:44 -0600

    added user controller

---
+++ www/app/components/user/user-controller.js
@@ -0,0 +1,33 @@
+import Store from "../../store/store.js";
+
+let store = new Store()
+
+let elem = document.getElementById('app')
+
+function draw() {
+  elem.innerHTML =
+    `
+  <div>
+    <h1>${store.state.user.username}</h1>
+  </div>
+  `
+
+}
+
+export default class UserController {
+  constructor() {
+
+  }
+
+  login(e) {
+    e.preventDefault();
+    let creds = {
+      username: e.target.username.value,
+      pin: e.target.pin.value
+    }
+    store.login(creds, draw)
+
+  }
+
+
+}
