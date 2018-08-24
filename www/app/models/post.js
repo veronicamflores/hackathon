43385586c314851ed4d2810d43eae9d0eb7fd3b7
Veronica Flores <veronica.marie.flores@gmail.com>
2018-08-24 13:49:54 -0600

    push

--- www/app/models/post.js
+++ www/app/models/post.js
@@ -5,4 +5,16 @@ export default class Post{
         this.imgUrl = data.ImgUrl
         this.userId = data.userId
     }
+
+    get listTemplate(){
+        return `
+        <div class="row">
+            <div class="col-4"><p>${this.userId.username}</p></div>
+            <div class="col-1"><button onclick="">X</button></div>
+            <div class="col-12"><img src="${this.imgUrl}"/></div>
+            <div class="col-12"><h5>${this.description}</h5></div>
+            <div class="col-4"><button onclick="">COMMENT</button></div>
+            <div class="col-2"><button onclick="">YAY</button><button onclick="">NAY</button></div>
+        </div>`
+    }
 }
